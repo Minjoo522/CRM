18dafaa7c021dbf8092d8040b65b48bc8cbca232
Minjoo <m0522inju@gmail.com>
2023-07-05 11:51:36 +0900

    💄 CSS-header

--- views/logout_view.py
+++ views/logout_view.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, session
+from flask import Blueprint, session, redirect, url_for
 
 bp = Blueprint('logout', __name__)
 
 @bp.route('/logout')
 def logout():
     session.pop('id', None)
+    return redirect(url_for('index'))
