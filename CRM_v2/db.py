083d142551546468f8ef37071ad9037e3719ede6
Minjoo <m0522inju@gmail.com>
2023-07-13 19:12:29 +0900

    ✨ user 검색기능 db 연결

--- CRM_v2/db.py
+++ CRM_v2/db.py
@@ -10,12 +10,12 @@ per_page = 10
 
 # 싱글톤?
 class DbController:
-    # 딕셔너리로 받아옴
+    # 딕셔너리로 받아옴 - 변수 이름 변경 필요
     conn = sqlite3.connect('database/crm.db', check_same_thread=False)
     conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
 
-    # 튜플로 받아옴
+    # 튜플로 받아옴 - 변수 이름 변경 필요
     conn2 = sqlite3.connect('database/crm.db', check_same_thread=False)
     cursor2 = conn2.cursor()
 
@@ -27,7 +27,8 @@ class DbController:
     def get_count(self, data_type):
         self.cursor2.execute('select count(*) from {}'.format(data_type))
         result = self.cursor2.fetchone()
-        return int(result[0])
+        total_pages = int(result[0]) // per_page + (int(result[0]) % per_page > 0)
+        return total_pages
     
     def page_item(self, data_type, page):
         self.cursor.execute('select * from {} limit {} offset ({}-1)*{}'.format(data_type, per_page, page, per_page))
@@ -38,6 +39,17 @@ class DbController:
         self.cursor.execute('select * from {} where id="{}"'.format(data_type, id))
         result = self.cursor.fetchone()
         return result
+    
+    def search_name_gender(self, data_type, search_name, search_gender, page):
+        self.cursor.execute(f'select * from {data_type} where name like "%{search_name}%" and gender like "%{search_gender}%" limit {per_page} offset ({page}-1)*{per_page}')
+        result = self.cursor.fetchall()
+        return result
+
+    def search_total_page(self, data_type, search_name, search_gender):
+        self.cursor2.execute(f'select count(*) from {data_type} where name like "%{search_name}%" and gender like "%{search_gender}%"')
+        result = self.cursor2.fetchone()
+        total_pages = int(result[0]) // per_page + (int(result[0]) % per_page > 0)
+        return total_pages
 
 # TODO: 나중에 삭제하기!
 def get_info(data_type):
--- CRM_v2/views/users_view.py
+++ CRM_v2/views/users_view.py
@@ -9,22 +9,16 @@ def users():
     page = request.args.get('page', default=1, type=int)
     search_name = request.args.get('name', default="", type=str)
     search_gender = request.args.get('gender', default="", type=str)
-    users = db_controller.get_info('user')
 
-    data = []
-    page_data = []
-
-    for user in users:
-        if search_name in user['Name']:
-            if search_gender in user['Gender']:
-                data.append(user)
-    
     keywords = "&name=" + search_name
     keywords += "&gender=" + search_gender
 
-    total_pages = db_controller.get_count('user')
-    # start_index = Pagination().get_start_index(page)
-    # end_index = Pagination().get_end_index(start_index)
-    # page_data = data[start_index:end_index] # 페이지 정보 받아오기
-    page_data = db_controller.page_item('user', page)
+    if search_name or search_gender:
+        # 페이징 처리 해주어야함 - 카운트
+        total_pages = db_controller.search_total_page('user', search_name, search_gender)
+        page_data = db_controller.search_name_gender('user', search_name, search_gender, page)
+    else:
+        total_pages = db_controller.get_count('user')
+        page_data = db_controller.page_item('user', page)
+
     return render_template("users.html", users = page_data, total_pages = total_pages, current_page = page, keywords = keywords, search_name=search_name, search_gender = search_gender)
