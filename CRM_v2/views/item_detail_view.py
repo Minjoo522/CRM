27f0565bf046ca64500bcd674baed43708b0f7ff
Minjoo <m0522inju@gmail.com>
2023-07-14 15:57:40 +0900

    ✨ item detail db와 연결

--- CRM_v2/views/item_detail_view.py
+++ CRM_v2/views/item_detail_view.py
@@ -1,11 +1,10 @@
 from flask import Blueprint, render_template
-from common.load_file import load_file
+from database.repositories.item_repository import Item
 
 bp = Blueprint('item_detail', __name__)
 
 @bp.route('/item_detail/<selected_id>/')
 def item_detail(selected_id):
-    items = load_file("src/item.csv")
-    for item in items:
-        if item['Id'] == selected_id:
-            return render_template("item_detail.html", item = item)
+    db = Item()
+    item = db.searh_by_id('item', selected_id)
+    return render_template("item_detail.html", item = item)
