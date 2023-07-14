3640c1c3fecc56a843bb2f06b7d68a8fd5594110
Minjoo <m0522inju@gmail.com>
2023-07-14 15:58:30 +0900

    ✨ store detail db와 연결

--- CRM_v2/views/store_detail_view.py
+++ CRM_v2/views/store_detail_view.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 from common.load_file import load_file
+from database.repositories.store_repository import Store
 
 bp = Blueprint('store_detail', __name__)
 
 @bp.route('/store_detail/<selected_id>/')
 def store_detail(selected_id):
-    stores = load_file("src/store.csv")
-    for store in stores:
-        if store['Id'] == selected_id:
-            return render_template("store_detail.html", store = store)
+    db = Store()
+    store = db.searh_by_id('store', selected_id)
+    return render_template("store_detail.html", store = store)
