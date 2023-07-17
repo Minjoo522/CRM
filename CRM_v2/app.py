4b972eedeb2b7568951f2ac2a8b3c5328f03fba6
Minjoo <m0522inju@gmail.com>
2023-07-17 18:52:07 +0900

    ✨ order_detail 페이지 추가

--- CRM_v2/app.py
+++ CRM_v2/app.py
@@ -7,7 +7,7 @@ app.secret_key = os.urandom(24)
 # views
 from views import items_view, login_view, logout_view,\
                     signup_view, users_view, user_detail_view, stores_view,\
-                    store_detail_view, item_detail_view, orders_view, order_items_view,\
+                    store_detail_view, item_detail_view, orders_view, order_detail_view,order_items_view,\
                     order_item_detail_view, index_view
 
 app.register_blueprint(login_view.bp)
@@ -20,6 +20,7 @@ app.register_blueprint(store_detail_view.bp)
 app.register_blueprint(items_view.bp)
 app.register_blueprint(item_detail_view.bp)
 app.register_blueprint(orders_view.bp)
+app.register_blueprint(order_detail_view.bp)
 app.register_blueprint(order_items_view.bp)
 app.register_blueprint(order_item_detail_view.bp)
 app.register_blueprint(index_view.bp)
