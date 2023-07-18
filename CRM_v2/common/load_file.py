b5566608ff4e20a29a8413e19529c9b423b2a7df
Minjoo <m0522inju@gmail.com>
2023-07-18 12:00:50 +0900

    ⚰️ db 도입으로 필요없어진 load_file, pagination.py 삭제

--- CRM_v2/common/load_file.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-def load_file(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        data_list = []
-        datas = csv.DictReader(file, skipinitialspace=True)
-        for data in datas:
-            data_list.append(data)
-    return data_list
