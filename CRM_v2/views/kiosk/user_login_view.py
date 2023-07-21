765359eb106b9ce7fd784dc2d8a1e8a87aea27c8
Minjoo <m0522inju@gmail.com>
2023-07-21 14:47:51 +0900

    🐛 user login, signup 관련 버그 수정 1. user_login : 없는 아이디로 로그인 시도시 Nontype 오류 해결 2. user_signup : 나이 계산 안되는 오류 해결

--- CRM_v2/views/kiosk/user_login_view.py
+++ CRM_v2/views/kiosk/user_login_view.py
@@ -16,9 +16,12 @@ def user_login():
         password = request.form['user-login-password']
 
         login_user_data = db.get_login_user_data('user', id_)
-        hash_password = login_user_data['LoginPassword']
-        if login_user_data and check_password_hash(hash_password, password):
-            return redirect(url_for('select_store.select_store', user_uuid = login_user_data['Id']))
-        error = '아이디와 비밀번호를 확인해주세요.'
+        try:
+            hash_password = login_user_data['LoginPassword']
+            if login_user_data and check_password_hash(hash_password, password):
+                return redirect(url_for('select_store.select_store', user_uuid = login_user_data['Id']))
+            error = '아이디와 비밀번호를 확인해주세요.'
+        except:
+            error = '아이디와 비밀번호를 확인해주세요.'
 
     return render_template("auth/user_login.html", error = error)
--- CRM_v2/views/kiosk/user_signup_view.py
+++ CRM_v2/views/kiosk/user_signup_view.py
@@ -1,38 +1,21 @@
 from flask import Blueprint, request, redirect, url_for, render_template
 from werkzeug.security import generate_password_hash
 import re
-import csv
 import uuid
 from datetime import datetime
-from common.load_file import load_file
 
 from database.repositories.signup_repeository import Signup
 
 bp = Blueprint('user_signup', __name__)
 
-# ✨ db 연결하기
-# db = SignUp()
-# 1. submit 버튼 눌렀을 때 중복된 아이디 확인
-#     select loginid for user where loginid = {inputid}
-#     if loginid:
-#       error = "중복된 아이디입니다."
-# 2. 비밀번호, 비밀번호 확인이 일치하는지 확인
-#     elif password1 != password2:
-#       error = "비밀번호가 일치하지 않습니다."
-# 3. 입력한 이름, 주소가 이상하지 않은지 확인하는 것도 필요
-#      고민...
-# ✅ 받아온 생년월일 이용해서 Age create 필요!
-# 4. 모든 정보가 이상 없으면, 비밀번호 해싱 + uuid 생성
-# 5. 해당 정보로 user db에 insert 해주기(튜플로 보내기)
-# ✅ 에러 각각!
 @bp.route('/user_signup/', methods=['GET', 'POST'])
 def user_signup():
     db = Signup()
-    # members = load_file("src/member.csv")
     id_error = None
     password_error = None
     name_error = None
     address_error = None
+
     if request.method == 'POST':
         sign_id = request.form['user_sign_id']
         password1 = request.form['user_password1']
@@ -48,11 +31,11 @@ def user_signup():
         name_max_length = 10
 
         current_date = datetime.now()
-        birth_date = datetime.strptime(birthdate, "%Y-%m-%d")
-        age = current_date.year - birth_date.year
-        if current_date.month < birth_date.month:
+        birth_date = birthdate.split('-')
+        age = current_date.year - int(birth_date[0])
+        if current_date.month < int(birth_date[1]):
             age -= 1
-        elif current_date.month == birth_date.month and current_date.day < birth_date.day:
+        elif current_date.month == int(birth_date[1]) and current_date.day < int(birth_date[2]):
             age -= 1
 
         address_pattern = re.compile(r'^[가-힣\d\s]+$')
@@ -72,18 +55,6 @@ def user_signup():
             address_error = '올바르지 않은 주소입니다.'
         else:
             db.insert_user_signup_data(user_id, name, gender, birthdate, age, address, sign_id, hash_password)
-            return redirect(url_for('login.login'))
+            return redirect(url_for('user_login.user_login'))
 
-        # ❌ csv ver
-        # if any(member["Id"] == sign_id for member in members):
-        #     error = '중복된 아이디입니다.'
-        # elif password1 != password2:
-        #     error = '비밀번호가 일치하지 않습니다.'
-        # else:
-        #     new_member = {"Id": sign_id, "Password": generate_password_hash(password1)}
-        #     with open("src/member.csv", "a", encoding="utf-8", newline="") as file:
-        #         fieldnames = ["Id", "Password"]
-        #         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        #         writer.writerow(new_member)
-            # return redirect(url_for('login.login'))
     return render_template("auth/user_signup.html", id_error = id_error, password_error = password_error, name_error = name_error, address_error = address_error)
